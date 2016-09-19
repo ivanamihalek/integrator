63d43fb50d951c96de57070f8c1474dfcabef6dc
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-09-19 01:01:52 -0400

    uniprot

---
+++ 01_uniprot_parser.pl
@@ -0,0 +1,70 @@
+#! /usr/bin/perl -w
+
+sub parse();
+
+@ARGV ||
+    die "Usage:  $0  <file name> \n";
+
+$filename = $ARGV[0];
+open (IF, "<$filename" ) 
+    || die "Cno $filename: $!.\n";
+
+$entry = "";
+while ( <IF> ) {
+    if (/^ID/) {
+	parse();
+	$entry = $_;
+    } else {
+	$entry .= $_;
+    }
+}
+parse();
+close IF;
+
+sub parse() {
+    $entry || return;
+    $entry =~ /\nOS   Homo sapiens/ || return;
+    #print $entry;
+    ($uniprot_id, $full_name,$gene_name, $tissue, $fn) = ("", "", "", "", "");
+    $reading_function  = 0;
+    foreach (split "\n", $entry) {
+	if  (/^ID/) {
+	} elsif (/^AC/) { 
+	    # it looks like the first iD is current, 
+            # and the rest are the older ids
+	    $_  =~ s/^AC//;
+	    ($uniprot_id)  = split ";";
+	    $uniprot_id =~ s/\s//g;
+	} elsif (/^DE   RecName: /) {
+	    $_  =~ s/^DE   RecName: //;
+	    ($full_name)  = split ";";
+	    $full_name  =~ s/Full=//g;
+	} elsif (/^GN   Name=/) {
+	    $_ =~ s/^GN   Name=//;
+	    ($gene_name) = split ";";
+	} elsif (/^RC   TISSUE/) {
+	    $_ =~ s/^RC   TISSUE=//;
+	    $tiss = $_;
+	    (substr $tiss, -1) eq ',' && chop $tiss;
+	    (substr $tiss, -1) eq ';' && ($tiss.";");
+	    ($tissue =~ $tiss) ||  ($tissue .= $tiss);	     
+	} elsif (/^CC/) {
+	    if (/\-!\-/) {
+		$reading_function  && last;
+		 
+		if (/FUNCTION/) {
+		    $reading_function  = 1;
+		    $_ =~ s/CC   -!- FUNCTION: //;
+		    $fn .= $_." ";
+		} 
+	    } elsif ( $reading_function )  {
+		$_ =~ s/CC      //;
+		$fn .= $_." ";
+	    } 	
+	} 
+    }
+    print "$uniprot_id\t$gene_name\t$full_name\t$tissue\t";
+    print "$fn\n";
+    #exit;
+
+}
---
+++ 05_cleanup_biogrid_table.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/perl
+
+while ( <>) {
+    chomp;
+    @aux = split "\t";
+    @new = ();
+    foreach (@aux) {
+	$blah = $_;
+	$blah =~ s/\s//g;
+	($blah eq '-') && ($blah='');
+	if (! $blah) {
+	    push @new, '\N';
+	} else {
+	    # somebody in BioGRID thought it would be cute to use backslash
+	    # as quotes
+	    $blah = $_;
+	    $blah =~ s/\\//g;
+	    push @new, $blah;
+	}
+    }
+
+    print join ("\t", @new), "\n";
+}
