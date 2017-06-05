137a56c71f02b5ad587cb0ff55ae7a3551612ddc
ivana <ivana.mihalek@gmail.com>
2017-06-05 18:10:23 -0400

    nonredundant ligands

--- 24_pdb/12_compile_model.py
+++ 24_pdb/12_compile_model.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 from integrator_utils.mysql import *
-import os
+import os, shutil, subprocess
+
+swissmodel_dir = "/databases/swissmodel"
+scratch_dir    = "/home/ivana/scratch"
+struct         = "/home/ivana/projects/enzyme_modeling/code/struct/struct"
+pdb_affine     = "/home/ivana/pypeworks/integrator/24_pdb/integrator_utils/pdb_affine_tfm.pl"
+geom_epitope   = "/home/ivana/pypeworks/integrator/24_pdb/integrator_utils/geom_epitope.pl"
+
+chain_pos = 21  #length 1
+res_name_pos = 17
+res_name_length = 3
+res_number_pos = 22
+res_number_length = 4
+aa_translation = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+                  'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+                  'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+                  'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
 
 ##########################################
 def parse_url(ur):
@@ -18,15 +34,6 @@ def parse_url(ur):
 ##########################################
 def pdb_to_dict_of_chars(path,filename):
 	sequence = {}
-	chain_pos =    21  #length 1
-	res_name_pos = 17
-	res_name_length = 3
-	res_number_pos = 22
-	res_number_length = 4
-	aa_translation = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
-     'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
-     'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
-     'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
 	#open file
 	infile = open (path+"/"+filename, "r")
 	for line in infile:
@@ -156,14 +163,119 @@ def find_model_clusters(model_info, path):
 	return cluster
 
 #########################################
-def extract_ligands(model):
-	return
+def extract_ligands(path, filename, scratch):
+
+	outfilename = "/".join([scratch, filename.replace('.pdb','')+".ligands.pdb"])
+
+	outfile = open(outfilename,"w")
+	infile  = open (path+"/"+filename, "r")
+	ligands = []
+	for line in infile:
+		if line[:6] != 'HETATM': continue
+		chain = line[chain_pos]
+		# I will take a peal of faith here and trut that Torsten and his buddies have maeked the
+		# the ligands (as opposed to modified residues) by setting the chain to "_"
+		if chain != "_": continue
+		resname = line[res_name_pos:res_name_pos+res_name_length].replace(' ','')
+		# http://www-structmed.cimr.cam.ac.uk/Course/Crystals/optimization.html
+		# popular crystallographic additives
+		# * Glycerol, which may stop nucleation and may give you fewer, larger crystals, and
+		# has the advantage of doubling as a cryo-protectant. "GOL","PGO","PGR"
+        # * Ethanol or dioxane, which have the effect of poisoning the crystals and stopping too much nucleation
+        # *Divalent cations like magnesium "EDO","EOH", "DIO
+        # * A detergent such as beta-octyl-glucoside "SOG","HTG"
+		if resname in ['HOH', "SO4", "GOL","PGO","PGR","EDO","EOH","DIO","SOG","HTG"]: continue
+		if not resname in ligands: ligands.append(resname)
+		outfile.write(line)
+	infile.close()
+	outfile.close()
+
+	return ligands, outfilename
+
+
 #########################################
-def align (model, anchor):
-	return
+def map_ligands_to_model (model_info, path, model, anchor, ligand_file_path, scratch):
+	struct_scratch = scratch + "/" + "struct_scratch"
+	shutil.rmtree(struct_scratch,ignore_errors=True)
+	os.makedirs(struct_scratch)
+	os.chdir(struct_scratch)
+
+	# find the alphabetically first chain in anchor
+	first_chain = sorted(model_info[anchor]['identical_pct'].keys())[0]
+	cmd = "%s -from %s -to %s -c2 %s" % (struct, path+"/"+model, path+"/"+anchor, first_chain)
+	subprocess.call(cmd, shell=True)
+	almt_file = "{}_to_{}{}.0.aln".format(model.replace('.pdb',''),anchor.replace('.pdb',''),first_chain)
+	if not os.path.exists(almt_file) or os.stat(almt_file).st_size == 0: return None
+	cmd = "grep tfm {} -A 3 | tail -n 3 | sed 's/{}//g' > tmp.tfm".format(almt_file,"%")
+	subprocess.call(cmd, shell=True)
+	if not os.path.exists('tmp.tfm') or os.stat('tmp.tfm').st_size == 0: return None
+	# TDOD check that the interesting ligands are really mapping to chain A, and not some place else
+	ligand_file_tfmd_path = ligand_file_path.replace('.pdb', '') + ".tfmd.pdb"
+
+	cmd = "{} {} tmp.tfm > {}".format(pdb_affine, ligand_file_path, ligand_file_tfmd_path)
+	subprocess.call(cmd, shell=True)
+
+	os.chdir(scratch)
+	shutil.rmtree(struct_scratch,ignore_errors=True)
+
+	return ligand_file_tfmd_path
+
 #########################################
-def transform(ligand_file, tfm_matrix_file):
-	return
+def merge_ligands(path, anchor, compiled_ligand_file_path, ligand_file_tfmd_path, scratch):
+	os.chdir(scratch)
+	# do I have anything closer than1 1 A?
+	#note that I am taking the footprint on the ligand, to remove clashing molecules
+	clashing_molecules = []
+	cmd = "{} {} {} 1.0".format(geom_epitope, ligand_file_tfmd_path, path+"/"+anchor)
+	ret = subprocess.check_output(cmd, shell=True)
+	for line in  ret.split("\n"):
+		if line.replace(" ","")=="": continue
+		clashing_molecules.append(line[:4].replace(" ",""))
+
+	cmd = "{} {} {} 1.0".format(geom_epitope, ligand_file_tfmd_path,compiled_ligand_file_path)
+	ret = subprocess.check_output(cmd, shell=True)
+	for line in  ret.split("\n"):
+		if line.replace(" ","")=="": continue
+		print line
+		clashing_molecules.append(line[:4].replace(" ",""))
+
+	# get rid of duplicates
+	clashing_molecules = list(set(clashing_molecules))
+	print ligand_file_tfmd_path, " clashing ", clashing_molecules
+
+	# remove clashing molecules from the file; renumber
+	outfile = open ("tmp_ligand.pdb","w")
+
+	# write the old file, find the last res number
+	infile = open(compiled_ligand_file_path,"r")
+	max_resnumber = -1
+	for line in infile:
+		resnumber = int(line[res_number_pos:res_number_pos+res_number_length])
+		outfile.write(line)
+		if max_resnumber<resnumber: max_resnumber=resnumber
+	infile.close()
+
+	infile = open(ligand_file_tfmd_path,"r")
+	prev_resnumber = -1
+	output_resno = max_resnumber
+	new_ligands = []
+	for line in infile:
+		# -1 will grab the chain id too
+		reslabel = line[res_number_pos-1:res_number_pos+res_number_length+1].replace(" ","")
+		if reslabel in clashing_molecules: continue
+		print reslabel , "---", clashing_molecules
+		resnumber = int(reslabel[1:])
+		if resnumber != prev_resnumber:
+			output_resno += 1
+			prev_resnumber = resnumber
+			new_ligands.append(line[res_name_pos:res_name_pos+res_name_length])
+		outfile.write(line[:res_number_pos] + "%4d"%output_resno + line[res_number_pos+4:])
+	infile.close()
+
+	outfile.close()
+	os.rename("tmp_ligand.pdb",compiled_ligand_file_path)
+	return new_ligands
+
 #########################################
 def compile_ligands():
 	return
@@ -173,7 +285,7 @@ def strip_and_glue (anchor, compiled_ligand_file):
 	return
 
 #########################################
-def compile_model( model_info, path, protein):
+def compile_model(model_info, path, protein):
 	print "compiling model for", protein
 	# find the anchor models
 	# there can be multiple models if they are non-overlapping
@@ -184,23 +296,37 @@ def compile_model( model_info, path, protein):
 		print anchor, anchored
 	# aligns structurally all models to their anchor
 	compiled_ligand_list = []
+	cwd = os.getcwd()
 	for anchor, anchored in cluster.iteritems():
+		os.chdir(cwd)
 		# lignads do no have a name that signs like a residue or modified residue
 		# ligand is not water and is not a crystallographic additive
-		anchor_ligand_file = extract_ligands(model)
+		scratch = scratch_dir + "/" + anchor.replace('.pdb','')
+		shutil.rmtree(scratch,ignore_errors=True)
+		os.makedirs(scratch)
+		anchor_ligands, anchor_ligand_file_path = extract_ligands(path, anchor, scratch)
+		compiled_ligand_list      = anchor_ligands
+		compiled_ligand_file_path = scratch + "/compiled_ligands.pdb"
+		shutil.copy(anchor_ligand_file_path,compiled_ligand_file_path)
 		for model in anchored:
-			ligands, ligand_file = extract_ligands(model)
-			compiled_ligand_list += ligands
-			if not ligand_file: continue # no ligands here
+			ligands, ligand_file_path = extract_ligands(path, model,scratch)
+			if not ligands or len(ligands)==0: continue # no ligands here
 			# use the transformation matrix to map all ligands to the anchor structure
-			tfm_matrix_file = align (model, anchor)
-			ligand_file_tfmd = transform(ligand_file, tfm_matrix_file)
-			# if any ligand overlaps with one already present in the anchor, drop it
-			# otherwise put it all in a new compiled ligand file
-			compiled_ligand_file = compile_ligands()
+			ligand_file_tfmd_path = map_ligands_to_model (model_info, path, model, anchor, ligand_file_path, scratch)
+			if not ligand_file_tfmd_path: continue
+			# remove clashing ligands adn ligands far from the main chain
+			# add ligands which are not clashing with the exisitng ones
+			new_ligands = merge_ligands(path, anchor, compiled_ligand_file_path, ligand_file_tfmd_path, scratch)
+			# remove clashing ligands adn ligands far from the main chain
+			# add ligands which are not clashing with the exisitng ones
+			compiled_ligand_list += new_ligands
+		print compiled_ligand_list
+		exit()
 		# strip anchor of all ligands
 		# put in the compiled ligand files - call the whole thing compiled_from_to
-		compiled_model = strip_and_glue (anchor, compiled_ligand_file)
+		# move out of scratch
+		compiled_model = strip_and_glue (anchor, compiled_ligand_file_path)
+		#shutil.rmtree(scratch,ignore_errors=True)
 	return compiled_model, compiled_ligand_list
 
 ##########################################
@@ -215,11 +341,15 @@ def store_distance_string (cursor, distance_string):
 
 ##########################################
 def main():
-	swissmodel_dir = "/databases/swissmodel"
+
+	for dependency in [swissmodel_dir, scratch_dir, struct, pdb_affine, geom_epitope]:
+		if os.path.exists(dependency): continue
+		print dependency, " not found"
+		exit()
+
 	swissmodel_meta_file = swissmodel_dir+"/index_human.csv"
 	model_info = parse_meta(swissmodel_meta_file) # there are more files (proteins/genes) here than qhat we will be using
 	db, cursor = connect()
-
 
 	#switch_to_db(cursor, "monogenic_development")
 	qry = "select * from monogenic_development.diseases"
---
+++ integrator_utils/geom_epitope.pl
@@ -0,0 +1,110 @@
+#! /usr/bin/perl -w 
+
+use strict;
+use warnings;
+my $CUTOFF_R = 5.0;
+
+(defined $ARGV[0] && defined $ARGV[1]  ) ||
+    die "usage: $0  <protein_pdb_file> <ligand_pdb_file> [<cutoff_distance>].\n";
+
+my $pdbfile1 = $ARGV[0];
+my $pdbfile2 = $ARGV[1];
+if ( defined $ARGV[2] ) {
+    $CUTOFF_R =  $ARGV[2];
+}
+my $inverse = 0;
+if ( defined $ARGV[3] ) {
+    $inverse = 1;
+}
+
+open (PDBFILE1, "<$pdbfile1") ||
+    die "could not open $pdbfile1 $!\n";
+
+open (PDBFILE2, "<$pdbfile2") ||
+    die "could not open $pdbfile2: $!\n";
+
+
+my $ctr = 0;
+my %atom_1_ctr = ();
+my %atom_1     = ();
+my @labels_1   = ();
+my @res_name_1 = ();
+my $res_label      = "";
+my $prev_res_label = "";
+while ( <PDBFILE1> ) {
+    /^ATOM/  || /^HETATM/ || next;
+    chomp;
+    my $serial = substr $_, 6, 5;  $serial =~ s/\s//g;
+    my $res_seq   = substr $_, 22, 4; $res_seq=~ s/\s//g;
+    my $chain  = substr $_, 21, 1;
+    my $x = substr $_,30, 8;  $x=~ s/\s//g;
+    my $y = substr $_,38, 8;  $y=~ s/\s//g;
+    my $z = substr $_, 46, 8; $z=~ s/\s//g;
+    $res_label= $chain.$res_seq;
+    if ( $res_label ne $prev_res_label ) {
+        if ($prev_res_label) {
+            $atom_1_ctr{$prev_res_label} = $ctr;
+            push @labels_1, $prev_res_label;
+        } 
+        
+        $ctr = 0;
+        $prev_res_label = $res_label;
+    }
+    $atom_1{$res_label}[$ctr][0] = $serial;
+    $atom_1{$res_label}[$ctr][1] = $x;
+    $atom_1{$res_label}[$ctr][2] = $y;
+    $atom_1{$res_label}[$ctr][3] = $z;
+    $res_name_1[$res_seq] = substr $_,  17, 3; $res_name_1 [$res_seq]=~ s/\s//g;
+    $ctr++;
+}
+$atom_1_ctr{$prev_res_label} = $ctr;
+
+$ctr = 0;
+my @atom_2 = ();
+while ( <PDBFILE2> ) {
+    /^ATOM/  || /^HETATM/ || next;
+    chomp;
+    my $serial = substr $_, 6, 5;  $serial =~ s/\s//g;
+    my $res_seq  = substr $_, 22, 4;  $res_seq=~ s/\s//g;
+    my $x = substr $_,30, 8;  $x=~ s/\s//g;
+    my $y = substr $_,38, 8;  $y=~ s/\s//g;
+    my $z = substr $_, 46, 8; $z=~ s/\s//g;
+    $atom_2[$ctr][0] = $serial;
+    $atom_2[$ctr][1] = $x;
+    $atom_2[$ctr][2] = $y;
+    $atom_2[$ctr][3] = $z;
+    $ctr++;
+}
+my $atom_2_ctr = $ctr;
+
+#****************************************************************
+foreach my $aa1 (@labels_1) {
+    defined $atom_1{$aa1}  || next;
+    my $nearest = 1000;
+    my ($x1, $y1, $z1, $x2, $y2, $z2);
+
+        for (my $ctr1=0; $ctr1<$atom_1_ctr{$aa1}; $ctr1++) {
+            $x1 = $atom_1{$aa1}[$ctr1][1];
+            $y1 = $atom_1{$aa1}[$ctr1][2];
+            $z1 = $atom_1{$aa1}[$ctr1][3];
+
+            for (my $ctr2=0; $ctr2<$atom_2_ctr; $ctr2++) {
+                $x2 = $atom_2[$ctr2][1];
+                $y2 = $atom_2[$ctr2][2];
+                $z2 = $atom_2[$ctr2][3];
+
+                my  $r = ($x1-$x2)*($x1-$x2)+ ($y1-$y2)*($y1-$y2)
+                    +($z1-$z2)*($z1-$z2);
+                $r = sqrt ($r);
+
+                if ($r <= $CUTOFF_R && $nearest > $r) {
+                    $nearest = $r;
+                }
+             }
+        }
+    
+
+    if ( $nearest <  1000 ) {
+        printf ("%s   %8.1f  \n", $aa1,  $nearest);
+    }
+}
