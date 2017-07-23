ee82f0682e1514fd6fb9461c7bd20115062ce98f
ivana <ivana.mihalek@gmail.com>
2017-07-23 12:14:04 -0400

    gnomad annotation

--- 12_exac/41_gnomad_mysql_tables.py
+++ 12_exac/41_gnomad_mysql_tables.py
@@ -8,14 +8,15 @@ def main():
 	db, cursor = connect()
 	for chrom in [str(i) for i in range(1,23)] + ['X','Y']:
 		table = "gnomad_freqs_chr_" + chrom
-		#qry = "DROP TABLE %s " % table
-		#search_db(cursor, qry, verbose=True)
+		qry = "DROP TABLE %s " % table
+		search_db(cursor, qry, verbose=True)
 		print table
 		qry  = "CREATE TABLE %s " % table
 		qry += " (position int(11), reference text,  variants text, consequences text, "
 		qry += " variant_counts text, total_count int, "
 		qry += " afr_counts text, afr_tot_count int, "
 		qry += " amr_counts text, amr_tot_count int, "
+		qry += " asj_counts text, asj_tot_count int, "
 		qry += " eas_counts text, eas_tot_count int, "
 		qry += " fin_counts text, fin_tot_count int, "
 		qry += " nfe_counts text, nfe_tot_count int, "
